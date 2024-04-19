abd765411430d06fb263180ca9ee03e5ea7ee2f1
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2024-04-19 10:37:19 +0530

    [+] First Commit

---
+++ runner.py
@@ -0,0 +1,33 @@
+# importing required libraries
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import time
+import os
+from itertools import combinations
+
+import pickle
+from os import path
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import confusion_matrix
+
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+# ---------------------------------------------
+
